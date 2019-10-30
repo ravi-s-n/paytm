fe993537a2034f8745c558a1c51897027b253e5a
ravi-s-n <55075301+ravi-s-n@users.noreply.github.com>
2019-10-30 12:55:14 +0530

    Update PaytmApplicationTests.java

--- src/test/java/com/example/paytm/PaytmApplicationTests.java
+++ src/test/java/com/example/paytm/PaytmApplicationTests.java
@@ -3,10 +3,10 @@ package com.example.paytm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class PaytmApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
